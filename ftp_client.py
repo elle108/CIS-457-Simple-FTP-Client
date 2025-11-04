014fcc4395eeaae64362fefc864e76bf456d5db1
elle108 <milleel7@mail.gvsu.edu>
2025-11-04 15:19:37 -0500

    variable name change

--- ftp_client.py
+++ ftp_client.py
@@ -36,6 +36,8 @@ data_socket = data_receptionist.accept()
 
 def open_connection(hostname):
     # Create the socket
+    global open_sock
+    
     open_sock = socket(AF_INET, SOCK_STREAM)
     open_sock.connect((hostname, 21))
     open_sock.recv_into(buffer)
@@ -58,18 +60,18 @@ def open_connection(hostname):
         print("Unknown response from server")
     
 def authenticate(username, password):
-    status_code = ftp_command(command_sock, f"USER {username}")
-    status_code = ftp_command(command_sock, f"PASS {password}")
+    status_code = ftp_command(open_sock, f"USER {username}")
+    status_code = ftp_command(open_sock, f"PASS {password}")
 def list_directory():
-    status_code = ftp_command(command_sock, "LIST")
+    status_code = ftp_command(open_sock, "LIST")
 def change_directory(path):
-    status_code = ftp_command(command_sock, f"CWD {path}")
+    status_code = ftp_command(open_sock, f"CWD {path}")
 def download_file(filename):
-    status_code = ftp_command(command_sock, f"RETR {filename}")
+    status_code = ftp_command(open_sock, f"RETR {filename}")
 def upload_file(filename):
-    status_code = ftp_command(command_sock, f"STOR {filename}")
+    status_code = ftp_command(open_sock, f"STOR {filename}")
 def close_connection():
-    status_code = ftp_command(command_sock, "QUIT")
+    status_code = ftp_command(open_sock, "QUIT")
 
 # Print options to user
 def print_menu():
